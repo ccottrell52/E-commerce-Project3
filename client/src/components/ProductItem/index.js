40eb7608070dd8333d8dc9c8e9407c7bcc4e6e5c
Courage Cottrell <104790429+ccottrell52@users.noreply.github.com>
2022-11-21 20:25:58 -0700

    Revert "Courage"

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_TO_WISHLIST } from "../../utils/mutations";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
 import { useStoreContext } from "../../utils/GlobalState";
@@ -7,19 +9,21 @@ import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
   const [state, dispatch] = useStoreContext();
-
+  const [addItem, { error }] = useMutation(ADD_TO_WISHLIST);
   const {
     image,
     name,
     _id,
     price,
-    quantity
+    quantity,
+    category
   } = item;
 
   const { cart } = state
 
   const addToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    console.log(itemInCart)
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
@@ -39,6 +43,21 @@ function ProductItem(item) {
     }
   }
 
+  const addToWishList = async ()=>{
+
+    try {
+      const { data } = await addItem({
+        variables: {
+          name,
+          price,
+          category
+        },
+      })
+    } catch ( error ){
+      console.error(error);
+    }
+  };
+
   return (
     <div className="card px-1 py-1">
       <Link to={`/products/${_id}`}>
@@ -52,7 +71,8 @@ function ProductItem(item) {
         <div>{quantity} {pluralize("item", quantity)} in stock</div>
         <span>${price}</span>
       </div>
-      <button onClick={addToCart}>Add to cart</button>
+      <button onClick={addToWishList}>Add to Wish List</button>
+      <button onClick={addToCart}>Add to Cart</button>
     </div>
   );
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,6 +33,7 @@ const typeDefs = gql`
     firstName: String
     lastName: String
     email: String
+    wishlist: [Product]
     orders: [Order]
   }
 
@@ -53,6 +54,7 @@ const typeDefs = gql`
     user: User
     order(_id: ID!): Order
     checkout(products: [ID]!): Checkout
+    wishlist: User
   }
 
   type Mutation {
@@ -61,6 +63,7 @@ const typeDefs = gql`
     updateUser(firstName: String, lastName: String, email: String, password: String): User
     updateProduct(_id: ID!, quantity: Int!): Product
     login(email: String!, password: String!): Auth
+    addToWishList(name: String!, price: Float!,  category: String!): User
   }
 `;
 
